8e905b9a374e7ad9baececa954ab0e2032223a4c
Jman5213 <jman5213@outlook.com>
2025-01-22 14:54:52 -0600

    Start page good! (I hope....)

--- GUI.py
+++ GUI.py
@@ -12,9 +12,13 @@ window = sg.Window('Chat', layout=Layouts.layout_one_base)
 
 
 def logout():
-    client.send(f'LGT {USERNAME}'.encode())
-    client.recv(1024).decode()
-    exit()
+    try:
+        client.send(f'LGT {USERNAME}'.encode())
+        client.recv(1024).decode()
+    except:
+        pass
+    finally:
+        exit()
 
 
 def connect_to_server(password):
@@ -29,7 +33,18 @@ def request_login(username, password):
     return client.recv(1024).decode()
 
 
+def request_create(username, password):
+    client.send(f'CTU {username} {password}'.encode())
+    return client.recv(1024).decode()
+
+
+def user_available(username):
+    pass
+
+
 def login_process():
+    global USERNAME
+
     window['-STARTFRAME-'].update(visible=False)
     window['-LOGINFRAME-'].update(visible=True)
     attempts = 0
@@ -38,36 +53,81 @@ def login_process():
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             logout()
-        elif event == '-USERNAME-' or event == '-PASSWORD-':
-            if window['-USERNAME-'].values != '' and window['-PASSWORD-'].values != '':
+        if event == '-LGNCANCEL-':
+            window['-STARTFRAME-'].update(visible=True)
+            window['-CREATEFRAME-'].update(visible=False)
+            window['-LOGINFRAME-'].update(visible=False)
+            return
+        if event == '-LGNUSERNAME-' or event == '-LGNPASSWORD-':
+            if values['-LGNUSERNAME-'] != '' and values['-LGNPASSWORD-'] != '':
                 window['-OK-'].update(disabled=False)
             else:
                 window['-OK-'].update(disabled=True)
-        elif event == '-OK-':
-            login = request_login(window['-USERNAME-'].values, window['-PASSWORD-'].values)  # True/False
+        if event == '-OK-':
+            login = request_login(values['-LGNUSERNAME-'], values['-LGNPASSWORD-'])  # True/False
+            if login:
+                USERNAME = values['-LGNUSERNAME-']
+                load_main_page()
 
 
 def create_account():
-    pass
+    window['-STARTFRAME-'].update(visible=False)
+    window['-CREATEFRAME-'].update(visible=True)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            logout()
+        if event == '-CRTCANCEL-':
+            window['-STARTFRAME-'].update(visible=True)
+            window['-CREATEFRAME-'].update(visible=False)
+            window['-LOGINFRAME-'].update(visible=False)
+            return
+        if event == '-CRTUSERNAME-' or event == '-CRTPASSWORD-' or event == '-CONFPASSWORD-':
+            if values['-CRTUSERNAME-'] != '' and values['-CRTPASSWORD-'] != '' and values['-CONFPASSWORD-'] != '':
+                if values['-CRTPASSWORD-'] != values['-CONFPASSWORD-']:
+                    window['-OK-'].update(disabled=True)
+                    window['-PASSERROR-'].update(visible=True)
+                    window['-ERROR-'].update(visible=False)
+                    window['-SERVERROR-'].update(visible=False)
+                    window['-CREATE-'].update(disabled=True)
+                elif not user_available(values['-CRTUSERNAME-']):
+                    window['-OK-'].update(disabled=True)
+                    window['-ERROR-'].update(visible=True)
+                    window['-PASSERROR-'].update(visible=False)
+                    window['-SERVERROR-'].update(visible=False)
+                    window['-CREATE-'].update(disabled=True)
+                else:
+                    window['-CREATE-'].update(disabled=True)
+        if event == '-OK-':
+            if request_create(values['-CRTUSERNAME-'], values['-CRTPASSWORD-']):
+                load_main_page()
+            else:
+                window['-OK-'].update(disabled=True)
+                window['-SERVERROR-'].update(visible=True)
+
 
 
 def load_main_page():
+    pass
+
+
+def load_start_page():
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             logout()
         elif event == '-LOGIN-':
             login_process()
-            break
         elif event == '-CREATEUSER-':
             create_account()
-            break
-            else:
-            window['-CREATEFRAME-'].update(visible=False)
+        else:
             window['-STARTFRAME-'].update(visible=True)
+            window['-CREATEFRAME-'].update(visible=False)
+            window['-LOGINFRAME-'].update(visible=False)
 
 
-load_main_page()
+load_start_page()
 
 """
 commands:
@@ -75,8 +135,10 @@ MSG <username> <chatid> <time/date> <message>       (message)
     MSGSENT
 LGN <username> <password>                           (login)
     SUCCESS/FAILURE
-CTU <username> <password>                          (create user)
+CTU <username> <password>                           (create user)
     CREATED
+UAV <username>                                      (username available)
+    TRUE/FALSE
 CTC <username> <chatsettings>                       (create chat)
     CREATED
 DLU <username> <password>                           (delete user)
--- Layouts.py
+++ Layouts.py
@@ -6,21 +6,26 @@ start_frame = [
 
 create_account_frame = [
     [sg.Push(), sg.Text("Please enter your information"), sg.Push()],
-    [sg.Push(), sg.Text('Invalid Username', colors='red', key='-ERROR-', visible=False), sg.Push()],
-    [sg.Push(), sg.Text("Username:"), sg.InputText(key='-USERNAME-', do_not_clear=True), sg.Push()],
-    [sg.Push(), sg.Text("Password:"), sg.InputText(key='-PASSWORD-', password_char='*', do_not_clear=True), sg.Push()],
-    [sg.Push(), sg.Text("Confirm Password:"), sg.InputText(key='-CONFPASSWORD-', password_char='*', do_not_clear=True),
+    [sg.Push(), sg.Text('Invalid Username', colors='red', key='-ERROR-', visible=False),
+     sg.Text("Passwords do not match", colors="red", key='-PASSERROR-', visible=False),
+     sg.Text("Server error, please try again", colors='red', key='-SERVERROR-', visible=False), sg.Push()],
+    [sg.Push(), sg.Text("Username:"), sg.InputText(key='-CRTUSERNAME-', enable_events=True), sg.Push()],
+    [sg.Push(), sg.Text("Password:"), sg.InputText(key='-CRTPASSWORD-', password_char='*', enable_events=True),
      sg.Push()],
-    [sg.Push(), sg.Button('Create Account', key='-CREATE-'), sg.Button('Cancel', key='-CANCEL-'), sg.Push()]
+    [sg.Push(), sg.Text("Confirm Password:"), sg.InputText(key='-CONFPASSWORD-', password_char='*', enable_events=True),
+     sg.Push()],
+    [sg.Push(), sg.Button('Create Account', key='-CREATE-', disabled=True), sg.Button('Cancel', key='-CRTCANCEL-'),
+     sg.Push()]
 ]
 
 login_frame = [
     [sg.Push(), sg.Text("Incorrect Username/Password combination", colors='red', key='-ERROR-', visible=False),
      sg.Push()],
-    [sg.Push(), sg.Text("Username:"), sg.InputText(key='-USERNAME-', do_not_clear=True, enable_events=True), sg.Push()],
-    [sg.Push(), sg.Text("Password:"), sg.InputText(key='-PASSWORD-', password_char='*', enable_events=True),
+    [sg.Push(), sg.Text("Username:"), sg.InputText(key='-LGNUSERNAME-', do_not_clear=True, enable_events=True),
      sg.Push()],
-    [sg.Button('Ok', disabled=True, key='-OK-'), sg.Button('Cancel', key='-CANCEL-')]
+    [sg.Push(), sg.Text("Password:"), sg.InputText(key='-LGNPASSWORD-', password_char='*', enable_events=True),
+     sg.Push()],
+    [sg.Button('Ok', disabled=True, key='-OK-'), sg.Button('Cancel', key='-LGNCANCEL-')]
 ]
 
 layout_one_base = [
